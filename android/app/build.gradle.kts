efefa8578f3c577441ac6137ebcdce9ac20db675
Gagan Deep S <gaganch17210@gmail.com>
2026-06-06 13:43:25 +0530

    Bump app versions to 1.4.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.zevclip.sender"
         minSdk = 26
         targetSdk = 36
-        versionCode = 13
-        versionName = "1.3.0"
+        versionCode = 14
+        versionName = "1.4.0"
     }
 
     buildTypes {
