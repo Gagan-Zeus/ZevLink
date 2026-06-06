50d487f88a663c547c514af9bbf958d28eb1b840
Gagan Deep S <gaganch17210@gmail.com>
2026-06-06 17:26:14 +0530

    Bump macOS and Android versions to 1.5.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.zevclip.sender"
         minSdk = 26
         targetSdk = 36
-        versionCode = 14
-        versionName = "1.4.0"
+        versionCode = 15
+        versionName = "1.5.0"
     }
 
     buildTypes {
